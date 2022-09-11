d498498ad77d365dffd81232ea910dc3f72df22c
Freddy Kwon <fredsckwon@gmail.com>
2022-09-11 11:46:41 -0400

    Updates made on homepage

--- src/components/pages/HomePage.js
+++ src/components/pages/HomePage.js
@@ -1,5 +1,5 @@
 import React, {useEffect} from 'react';
-import Shape from '../imgs/outnoww.png'
+import Shape from '../imgs/Asset 30.png'
 import './HomePage.css'; 
 import Hover from 'react-3d-hover';
 
