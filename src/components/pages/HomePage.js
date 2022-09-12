d9e295fc81f8bc1987337ef9f90ea1c9925b90fc
Freddy Kwon <fredsckwon@gmail.com>
2022-09-12 00:17:30 -0400

    Updated landing image

--- src/components/pages/HomePage.js
+++ src/components/pages/HomePage.js
@@ -1,17 +1,22 @@
 import React, {useEffect} from 'react';
-import Shape from '../imgs/Asset 30.png'
+import Shape from '../imgs/landing.gif';
 import './HomePage.css'; 
 import Hover from 'react-3d-hover';
 
 const Home = () => {
     return (
+        <>
         <div class="columns is-centered">
-            <Hover speed={500} perspective={800} max={25}>
-                <a href="https://linktr.ee/freddyboythings" target="_blank">
+            <Hover speed={300} perspective={3000} max={25}>
+                <a href="https://linktr.ee/freddyboythings">
                     <img className="cover"src={Shape} />
                 </a>
             </Hover>
         </div>
+        {/* <a href="https://linktr.ee/freddyboythings">
+            <p id="clickHere">CLICK TO LISTEN</p>
+        </a> */}
+        </>
     )
 }
 
